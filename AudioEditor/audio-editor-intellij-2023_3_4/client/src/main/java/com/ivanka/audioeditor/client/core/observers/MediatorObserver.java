3053e71f39bea185218a849d2cb573bb93b6f6f6
ivannakotyk <ivannakotik3@gmail.com>
2025-11-07 16:53:16 +0200

    Лабораторна робота №7

--- AudioEditor/audio-editor-intellij-2023_3_4/client/src/main/java/com/ivanka/audioeditor/client/core/observers/MediatorObserver.java
+++ AudioEditor/audio-editor-intellij-2023_3_4/client/src/main/java/com/ivanka/audioeditor/client/core/observers/MediatorObserver.java
@@ -4,9 +4,6 @@ import com.ivanka.audioeditor.client.core.Observer;
 import com.ivanka.audioeditor.client.core.events.EditorEvent;
 import com.ivanka.audioeditor.client.core.mediator.EditorMediator;
 
-/**
- * Єдиний спостерігач-місток: усі EditorEvent потрапляють у медіатор рівно ОДИН раз.
- */
 public class MediatorObserver implements Observer {
     private final EditorMediator mediator;
     public MediatorObserver(EditorMediator mediator) { this.mediator = mediator; }
